b857f1558e8779e5ca25055c50b12131e92fa657
黃健旻 <yfr.huang@hotmail.com>
2022-07-27 02:07:56 +0800

    build: update build (#10)

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-rm -rf dist
-
 rm -rf /var/www/html/css
 rm -rf /var/www/html/img
 rm -rf /var/www/html/js
 rm -f /var/www/html/favicon.svg
 rm -f /var/www/html/index.html
 
-mv css /var/www/html
-mv img /var/www/html
-mv js /var/www/html
-mv favicon.svg /var/www/html
-mv index.html /var/www/html
+mv dist/css /var/www/html
+mv dist/img /var/www/html
+mv dist/js /var/www/html
+mv dist/favicon.svg /var/www/html
+mv dist/index.html /var/www/html

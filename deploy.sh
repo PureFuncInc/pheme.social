5ea9cb16e2a60134fffd416a22155b9bb5c0fc97
黃健旻 <yfr.huang@hotmail.com>
2022-07-27 13:42:35 +0800

    :construction_worker: ci: add conosle log version (#12)

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ rm -rf /var/www/html/js
 rm -f /var/www/html/favicon.svg
 rm -f /var/www/html/index.html
 
+sed -i "s/<body>/<body><script>console.log(\"> pheme --version\");console.log(\"$GITHUB_REF_NAME\");<\/script>/g" index.html
+
 mv dist/css /var/www/html
 mv dist/img /var/www/html
 mv dist/js /var/www/html
